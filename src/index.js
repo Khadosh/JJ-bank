30afdbeeb3f32915d94d5e39d95b6559532d4072
Cris T <crisostomotorres@hotmail.com>
2020-02-17 10:42:33 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap';
+import Swal from 'sweetalert2';
+
 import './index.scss';
 
 const title = document.getElementById('title');
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,75 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var $ = require("jquery");
+// The current card.js code does not explicitly require jQuery, but instead uses the global, so this line is needed.
+window.jQuery = $;
+var card = require("card");
 
 module.exports = {
-  entry: {
-    main: "./src/index.js"
-  },
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: [["@babel/plugin-proposal-class-properties"]]
-          }
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      },
-      {
-        test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
-      }
+    entry: {
+        main: "./src/index.js"
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "build")
+    },
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                        plugins: [
+                            ["@babel/plugin-proposal-class-properties"]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run postcss actions
+                    options: {
+                        plugins: function() { // postcss plugins, can be exported to postcss.config.js
+                            return [
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css"
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "styles.css"
-    })
-  ]
 };
