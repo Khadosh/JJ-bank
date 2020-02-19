22324a6a9fc54555aa765711aa5a2af70150c5de
CrisT <crisostomotorres@hotmail.com>
2020-02-19 17:59:53 -0300

    Fixed a couple issues regarding the card not working properly

--- src/Initialitation/Card.js
+++ src/Initialitation/Card.js
@@ -1,6 +1,39 @@
-new Card({
-    form: document.querySelector('form'),
-    container: '.card-wrapper'
+var card = new Card({
+    // a selector or DOM element for the form where users will
+    // be entering their information
+    form: 'form', // *required*
+    // a selector or DOM element for the container
+    // where you want the card to appear
+    container: '.card-wrapper', // *required*
+
+    formSelectors: {
+        numberInput: 'input#number', // optional — default input[name="number"]
+        expiryInput: 'input#expiry', // optional — default input[name="expiry"]
+        cvcInput: 'input#cvc', // optional — default input[name="cvc"]
+        nameInput: 'input#name' // optional - defaults input[name="name"]
+    },
+
+
+    // Strings for translation - optional
+    messages: {
+        validDate: 'valid\ndate', // optional - default 'valid\nthru'
+        monthYear: 'mm/yyyy', // optional - default 'month/year'
+    },
+
+    /* Default placeholders for rendered fields - optional
+    placeholders: {
+        number: '•••• •••• •••• ••••',
+        name: 'Full Name',
+        expiry: '••/••',
+        cvc: '•••'
+    },*/
+
+    /*masks: {
+        cardNumber: '•' // optional - mask card number
+    },*/
+
+    // if true, will log helpful messages for setting up Card
+    debug: false // optional - default false
 });
 
 export default card
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import 'bootstrap';
 import Swal from 'sweetalert2';
 import './../node_modules/card/dist/card';
-import card from './Initialitation/Card';
+import Card from './Initialitation/Card';
 
 import './index.scss';
+/*let card = new Card({
+    form: document.querySelector('form'),
+    container: '.card-wrapper',
+
+    formSelectors: {
+        numberInput: 'input#number', // optional — default input[name="number"]
+        expiryInput: 'input#expiry', // optional — default input[name="expiry"]
+        cvcInput: 'input#cvc', // optional — default input[name="cvc"]
+        nameInput: 'input#name' // optional - defaults input[name="name"]
+    },
+}); */
